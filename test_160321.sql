1c6c6eb6ed6003ce75d8277579a56348bff81add
eunyoungkim220 <mnieyk@naver.com>
2016-03-21 11:29:26 +0900

    실습1_160321

--- test_160321.sql
+++ test_160321.sql
@@ -9,13 +9,3 @@ FROM
 GROUP BY 1
 ;
 
-# Cohort Size
-DROP TEMPORARY TABLE IF EXISTS cohort_size;
-CREATE TEMPORARY TABLE cohort_size
-SELECT
- LEFT(first_time, 7) Month,
- COUNT(*) num
-FROM
- first_rental
-GROUP BY 1
-;
