81abd3cf23d2c0e57d3f4fa12fc7488acb22007d
eunyoungkim220 <mnieyk@naver.com>
2016-03-21 11:27:36 +0900

    실습2_160321

--- test_160321.sql
+++ test_160321.sql
@@ -1,8 +1,21 @@
-
+# Customer's First Rental
+DROP TEMPORARY TABLE IF EXISTS first_rental;
+CREATE TEMPORARY TABLE first_rental
 SELECT
  r.customer_id,
- MIN(r.rental_date)
+ MIN(r.rental_date) first_time
 FROM
  rental r 
+GROUP BY 1
+;
+
+# Cohort Size
+DROP TEMPORARY TABLE IF EXISTS cohort_size;
+CREATE TEMPORARY TABLE cohort_size
+SELECT
+ LEFT(first_time, 7) Month,
+ COUNT(*) num
+FROM
+ first_rental
 GROUP BY 1
 ;
